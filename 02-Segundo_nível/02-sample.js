6dd00c00b762cfcefb8ea0ab82a4b04301354f4a
Cauã Milfont <casllostreetwear12@gmail.com>
2024-11-07 16:21:07 -0300

    Correção pasta 2 arquivo 2

--- 02-Segundo_nível/02-sample.js
+++ 02-Segundo_nível/02-sample.js
@@ -1,9 +1,26 @@
 function verificarSePodeDirigir(idade) {
+    if(idade < 0){
+        throw new Error('O número não pode ser negativo');
+    }
     if (idade >= 18) {
         return "Pode dirigir";
     } else {
         return "Não pode dirigir";
     }
 }
-
-console.log(verificarSePodeDirigir(16));
+try{
+console.log(verificarSePodeDirigir(-1));
+}catch (error) {
+    console.error(error.message);
+}
+try{
+    console.log(verificarSePodeDirigir(18));
+  
+    }catch (error) {
+        console.error(error.message);
+    }
+    try{
+        console.log(verificarSePodeDirigir(15));
+        }catch (error) {
+            console.error(error.message);
+        }
