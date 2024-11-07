027c8174072be0c6a612a3d05bac9cd345d0b065
LucasBrandao934 <lucasemanuel1234@hotmail.com>
2024-11-06 21:25:29 -0300

    Primeiro arquivo corrigido

--- 01-Primeiro nível/01-sample.js
+++ 01-Primeiro nível/01-sample.js
@@ -1,13 +1,13 @@
-function h(a) {
-    let b = [];
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] % 2 === 0) {
-            b.push(a[i]);
+function pegarPar(lista) {
+    let listaPar = [];
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i] % 2 === 0) {
+            listaPar.push(lista[i]);
         }
     }
-    return b;
+    return listaPar;
 }
 
-let x = [1, 2, 3, 4, 5, 6];
-let y = h(x);
-console.log(y);
+let lista = [1, 2, 3, 4, 5, 6];
+let listaPar = pegarPar(lista);
+console.log(listaPar);
