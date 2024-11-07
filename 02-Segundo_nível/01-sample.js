03019a1b7a9f2b44c8a88b1a891f4ba39ad30853
Cauã Milfont <casllostreetwear12@gmail.com>
2024-11-07 16:01:46 -0300

    Correção pasta 2 arquivo 1

--- 02-Segundo_nível/01-sample.js
+++ 02-Segundo_nível/01-sample.js
@@ -1,6 +1,19 @@
 function calcularDesconto(preco) {
+    if(preco < 0){
+        throw new Error('O número não pode ser negativo');
+    }
     return preco - (preco * 0.15);
 }
 
-const precoFinal = calcularDesconto(100);
-console.log(`Preço com desconto: ${precoFinal}`);
+try {
+    const precoFinal = calcularDesconto(150); 
+    console.log(`Preço com desconto: ${precoFinal}`);
+} catch (error) {
+    console.error(error.message); 
+}
+try {
+    const precoFinal = calcularDesconto(-1); // Esse número negativo vai lançar um erro
+    console.log(`Preço com desconto: ${precoFinal}`);
+} catch (error) {
+    console.error(error.message); // Isso vai exibir a mensagem de erro
+}
