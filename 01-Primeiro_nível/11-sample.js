1cec0c1237cf0eaf01b695c77521d348ec37c022
Cauã Milfont <casllostreetwear12@gmail.com>
2024-11-07 11:26:57 -0300

    Correção pasta 1 arquivo 11

--- 01-Primeiro_nível/11-sample.js
+++ 01-Primeiro_nível/11-sample.js
@@ -1,25 +1,25 @@
-class z {
-    constructor(a, b, c, d, e, f, g, h) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
-        this.e = e;
-        this.f = f;
-        this.g = g;
-        this.h = h;
+class Usuario {
+    constructor(nome, idade, papel, criadoEm, ultimoLogin, ativo, tentativasLogin, id) {
+        this.nome = nome;
+        this.idade = idade;
+        this.papel = papel;
+        this.criadoEm = criadoEm;
+        this.ultimoLogin = ultimoLogin;
+        this.ativo = ativo;
+        this.tentativasLogin = tentativasLogin;
+        this.id = id;
     }
 
-    x() {
-        if (this.f) {
+    VerificarStatusAtivo() {
+        if (this.ativo) {
             console.log("Ativo");
         } else {
             console.log("Inativo");
         }
     }
 
-    y() {
-        if (this.c === "admin") {
+    verificarAcesso() {
+        if (this.papel === "admin") {
             console.log("Admin pode acessar todas as áreas.");
         } else {
             console.log("Convidado tem acesso limitado.");
@@ -28,19 +28,19 @@ class z {
 }
 
 // Deixe a função clara para que seja possível de compreender que ela verifica o número de usuários com a role de admin
-function t(users) {
-    let c = 0;
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].c === "admin") {
-            c++;
+function contarAdmins(usuarios) {
+    let contador= 0;
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].papel=== "admin") {
+            contador++;
         }
     }
-    return c;
+    return contador;
 }
 
 // Deixe a função clara para que seja possível de compreender que ela verifica se o número de tentativas de login é excessivo
-function l(a, b) {
-    if (a.h > b) {
+function verificarTentativasLogin(usuarios, limite) {
+    if (usuario.tentativasLogin > limite) {
         console.log("Tentativas de login excessivas.");
     } else {
         console.log("Tentativas de login sob controle.");
@@ -48,38 +48,39 @@ function l(a, b) {
 }
 
 // Deixe a função clara para que seja possível de compreender que ela verifica qual usuário logou mais recentemente
-function c(u1, u2) {
-    if (u1.e > u2.e) {
-        return `${u1.a  } logou mais recentemente.`;
+function obterUltimoLogin(usuario1, usuario2) {
+    if (usuario1.ultimoLogin > usuario2.ultimoLogin) {
+        return `${usuario1.nome  } logou mais recentemente.`;
     } else {
-        return `${u2.a  } logou mais recentemente.`;
+        return `${usuario2.nome  } logou mais recentemente.`;
     }
 }
 
 // Deixe a função clara para que seja possível de compreender que ela verifica se o usuário foi criado recentemente
-function r(user) {
+function verificaUsuarioRecente(usuario) {
     const now = new Date();
-    if (now - user.d < 31536000000) {
+    const umAnoEmMillis = 31536000000 // 1 ano em milissegundos :)
+    if (now - usuario.criadoEm < umAnoEmMillis) {
         console.log("Usuário criado recentemente.");
     } else {
         console.log("Usuário antigo.");
     }
 }
 
-const usrs = [
-    new z("Carlos", 25, "admin", new Date(2023, 1, 15), new Date(2024, 8, 1), true, 100, 2),
-    new z("Ana", 30, "guest", new Date(2020, 4, 22), new Date(2024, 7, 31), true, 50, 3),
-    new z("José", 29, "admin", new Date(2022, 10, 5), new Date(2024, 6, 10), false, 200, 5),
-    new z("Maria", 35, "guest", new Date(2021, 2, 10), new Date(2023, 12, 25), false, 80, 7)
+const usuario = [
+    new Usuario("Carlos", 25, "admin", new Date(2023, 1, 15), new Date(2024, 8, 1), true, 100, 2),
+    new Usuario("Ana", 30, "guest", new Date(2020, 4, 22), new Date(2024, 7, 31), true, 50, 3),
+    new Usuario("José", 29, "admin", new Date(2022, 10, 5), new Date(2024, 6, 10), false, 200, 5),
+    new Usuario("Maria", 35, "guest", new Date(2021, 2, 10), new Date(2023, 12, 25), false, 80, 7)
 ];
 
-console.log(`Usuários admin: ${  t(usrs)}`);
+console.log(`Usuários admin: ${  contarAdmins(usuarios)}`);
 
-usrs[0].x();
-usrs[1].y();
+usuarios[0].verificarStatusAtivo();
+usuarios[1].verificaAcesso();
 
-l(usrs[2], 4);
+verificarTentativasLogin(usuarios[2], 4);
 
-console.log(c(usrs[0], usrs[3]));
+console.log(obterUltimoLogin(usuarios[0], usuarios[3]));
 
-r(usrs[3]);
+verificaUsuariosRecentes(usuarios[3]);
