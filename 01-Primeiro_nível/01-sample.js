5b28cb91c6a7b3e68775ef1c77ca023059356b8d
Ismaelmarinho <ismaelmarinho0810@gmail.com>
2024-11-07 14:16:23 -0300

    correção dos números mágicos

--- 01-Primeiro_nível/01-sample.js
+++ 01-Primeiro_nível/01-sample.js
@@ -1,27 +1,13 @@
-<<<<<<< HEAD
-function h(a) {
-    const b = [];
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] % 2 === 0) {
-            b.push(a[i]);
-=======
 function pegarPar(lista) {
-    let listaPar = [];
+    const listaPar = [];
     for (let i = 0; i < lista.length; i++) {
         if (lista[i] % 2 === 0) {
             listaPar.push(lista[i]);
->>>>>>> 1f495b95e2eceffa80a8c5bf417389cb36371fd7
         }
     }
     return listaPar;
 }
 
-<<<<<<< HEAD
-const x = [1, 2, 3, 4, 5, 6];
-const y = h(x);
-console.log(y);
-=======
-let lista = [1, 2, 3, 4, 5, 6];
-let listaPar = pegarPar(lista);
+const lista = [1, 2, 3, 4, 5, 6];
+const listaPar = pegarPar(lista);
 console.log(listaPar);
->>>>>>> 1f495b95e2eceffa80a8c5bf417389cb36371fd7
