8a16070c07156558d343a9c7f03a1a59f287333d
Cauã Milfont <casllostreetwear12@gmail.com>
2024-11-09 10:20:09 -0300

    Correção pasta 2 arquivo 4

--- 02-Segundo_nível/04.sample.js
+++ 02-Segundo_nível/04.sample.js
@@ -1,8 +1,12 @@
 function tentarLogin(senha) {
-    let tentativas = 0;
+    const tentativasMaximas = 3;
     const senhaCorreta = "12345";
-
-    while (tentativas < 3) {
+    if (tentativasMaximas <= 0){
+        throw new Error("O número de tentativas tem que ser maior que o")
+    
+    }
+//Troquei o while pelo for pois foi usar para contar tentativas
+    for (let tentativas = 0; tentativas < tentativasMaximas;) {
         if (senha === senhaCorreta) {
             return "Login efetuado com sucesso!";
         }
@@ -11,5 +15,8 @@ function tentarLogin(senha) {
 
     return "Tentativas de login excedidas.";
 }
-
-console.log(tentarLogin("123"));
+try{
+console.log(tentarLogin("12345"));
+} catch (error){
+    console.log(error.message);
+}
